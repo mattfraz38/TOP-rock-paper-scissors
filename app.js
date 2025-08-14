e34211dc47d8d4cc601add11bd88e8b39470468d
Matt Frazier <frazier3388@yahoo.com>
2025-08-15 08:58:31 +0900

    Add computer choice logic
    Generate a random number between 1 and 3.
    Assign string values of "Rock", "Scissors", and
    "Paper" to the number values respectively.
    Then return the random computer choice.

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 "use strict";
 
 console.log("Hello World");
+
+// Generate a random number between 1 and 3 to represent the choices "Rock",
+// "Scissors", or "Paper" then return that string value
+let getComputerChoice = () => {
+  let randomNumber = Math.ceil(Math.random() * 3);
+
+  switch (randomNumber) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
+};
+
+for (let i = 0; i < 5; i++) {
+  console.log(getComputerChoice());
+}
