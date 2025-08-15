ba1631ef525a14acce88707bd3b2937b7f729e8b
Matt Frazier <frazier3388@yahoo.com>
2025-08-15 10:05:22 +0900

    Add logic to play a single round
    Correct return values for computer choice
    so that both computer and user choces return
    number values for comparison. Add a loop to
    play five rounds and alert the user of the final
    score.

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
 "use strict";
 
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
 
 // Generate a random number between 1 and 3 to represent the choices "Rock",
-// "Scissors", or "Paper" then return that string value
+// "Scissors", or "Paper" then return that number value
 let getComputerChoice = () => {
   let randomNumber = Math.ceil(Math.random() * 3);
 
-  switch (randomNumber) {
-    case 1:
-      return "Rock";
-    case 2:
-      return "Paper";
-    case 3:
-      return "Scissors";
+  return randomNumber;
+};
+
+// Prompt the user for their choice of "Rock, Scissors, or Paper" then 
+// return the number value of their choice
+let getHumanChoice = () => {
+  let userChoice = prompt("Rock, Scissors, or Paper?");
+
+  switch (userChoice.toLowerCase()) {
+    case "rock":
+      return 1;
+    case "paper":
+      return 2;
+    case "scissors":
+      return 3;
   }
 };
 
-for (let i = 0; i < 5; i++) {
-  console.log(getComputerChoice());
-}
-
-// Prompt the user for their choice of "Rock, Scissors, or Paper" then 
-// return the value of their choice
-let getHumanChoice = () => {
-  return prompt("Rock, Scissors, or Paper?");
+// Compare the number value of the user and computer choices and alert the user
+// which is larger marking the winner then increment the winner score by one
+let playRound = (computerChoice, userChoice) => {
+  if (computerChoice > userChoice) {
+    alert("Computer Wins!");
+    computerScore += 1;
+  } else if (userChoice > computerChoice) {
+    alert("User Wins!");
+    humanScore += 1;
+  } else {
+    alert("It's a tie!");
+    computerChoice = getComputerChoice();
+    computerChoice = getComputerChoice();
+  }
 };
 
-console.log(getHumanChoice());
+// play five rounds of rock, paper, scissors
+for (let i = 0; i < 5; i++) {
+  playRound(getComputerChoice(), getHumanChoice());
+}
+
+// alert the user of the final score after five rounds
+alert(`User Score: ${humanScore}\nComputer Score: ${computerScore}`);
